c64c81c09be5a51133ada743e91b2bde2474c155
namih1n1 <alexandre.hoainam.le@gmail.com>
2014-12-16 09:35:32 +0100

    Inclusion du header

--- actor_birth.php
+++ actor_birth.php
@@ -1,44 +1,35 @@
 <?php
-require_once( "sparqllib.php" )
-
-$db = sparql_connect( "http://dbpedia.org/sparql" );
-if( !$db ) { print sparql_errno() . ": " . sparql_error(). "\n"; exit; }
-
-sparql_ns("dbpedia-owl","http://dbpedia.org/ontology/");
-
-$today = date("m-d");
-
 $sparql = "
 	select distinct ?nom where {
 		?Ressource dbpprop:occupation ?occupation .
 		?Ressource dbpedia-owl:birthDate ?naissance .
 		?Ressource rdfs:label ?nom .
 		FILTER(?occupation like \"*Actor*\") .
-		FILTER(?naissance like \"*-".$today."\") .
+		FILTER(?naissance like \"*-".$__today."\") .
 		FILTER langmatches(lang(?nom),\"en\")
 	}";
 	
 $result = sparql_query( $sparql );
-if( !$result ) { print sparql_errno() . ": " . sparql_error(). "\n"; exit; }
+if( !$result ) { echo sparql_errno() . ": " . sparql_error(). "\n"; exit; }
 
 $fields = sparql_field_array( $result );
 
-print sparql_num_rows( $result )." acteurs sont n&eacute;s ce jour.</p>";
-print "<table class='example_table'>";
-print "<tr>";
+echo sparql_num_rows( $result )." acteurs sont n&eacute;s ce jour.</p>";
+echo "<table class='example_table'>";
+echo "<tr>";
 
 // Entête
-// foreach( $fields as $field ) {	print "<th>$field</th>"; }
+// foreach( $fields as $field ) {	echo "<th>$field</th>"; }
 
-print "</tr>";
+echo "</tr>";
 while( $row = sparql_fetch_array( $result ) )
 {	
-	print "<tr>";
+	echo "<tr>";
 		foreach( $fields as $field )
 		{		
-			echo "<td><a href=\"http://wikipedia.org/wiki/".utf8_decode("$row[$field]")."\"> ".utf8_decode("$row[$field]"). "</td>";	
+			echo "<td><a href=\"".$__url_wiki. utf8_decode("$row[$field]")."\"> ".utf8_decode("$row[$field]"). "</td>";	
 		}
-	print "</tr>";
+	echo "</tr>";
 }
-print "</table>";
+echo "</table>";
 ?>
